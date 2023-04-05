026a3d5ab381cca2a748dbd29a48ac16050e4b8b
Naveen Bhusare <79650422+Naveen-7000@users.noreply.github.com>
2023-04-05 20:08:45 +0530

    browserrouter replaced by hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React,{ useState } from 'react';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "../src/Pages/Home/Home";
 import Analytics from './Pages/Analytics/Analytics';
 import './App.css'
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <nav>
-          <a className='navitem' href={'/'}>Home</a>
-          <a className='navitem' href={'/analytics'}>Analytics</a>
+          <a className='navitem' href={'#/'}>Home</a>
+          <a className='navitem' href={'#/analytics'}>Analytics</a>
         </nav>
         <Router>
           <Routes>
